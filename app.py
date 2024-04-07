afbcdaa8ffc6029ee4fc9b9e97081bb011a03d14
Ayibongwe Ndhlovu <54669439+Ayibongwe02@users.noreply.github.com>
2024-04-07 22:04:52 +0000

    Added Html and CSS to the Career project

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template 
 
 app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def hello_world():
-  return "Hello, Ayibongwe"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
